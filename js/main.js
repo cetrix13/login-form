a77f106d9b751cb4ba3b8e2d8f2c55c4408e811c
Alexandr Dadykin <DadykinAV@yandex.ru>
2019-02-19 23:38:16 +0300

    Add Vue.js and validation of phone and password

---
+++ js/main.js
@@ -0,0 +1,48 @@
+new Vue({
+    el:'#app',
+    data: {
+        pass: '',
+        phone: '',
+    },
+    methods: {
+        submitForm: function(event) {
+            $.ajax({
+                url: event.target.action,
+                method: "GET",
+                data: { phone: this.phone, pass: this.pass },
+                success: function(data) {
+                    // TODO: Add proper handlers
+                    alert("All good and data send");
+                },
+                error: function(error) {
+                    // TODL: Add proper handlers
+                    alert("Error occuried");
+                },
+                complete: function() {
+                    event.target.reset();
+                }
+            });
+            this.phone = ''
+            this.pass = '';
+        }
+
+    },
+    computed: {
+        isValidForm: function() {
+            return this.isValidPhone && this.isValidPass;
+        },
+        isValidPhone: function() {
+            if (this.phone.length > 0) {
+                if (/^\d{1}\s\(\d{3}\)\s\d{2}-\d{2}-\d{2}$/.test(this.phone.trim())) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        },
+        isValidPass: function() {
+            return this.pass.length > 5;
+        }
+    },
+});
